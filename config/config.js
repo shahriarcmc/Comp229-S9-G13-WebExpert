b3ee65a16c3f69d49157844405c0bf7515008631
Shahriar Moinuddin <shahriar.cmc@gmail.com>
2021-12-15 02:17:22 -0500

    MONGODB pw

--- config/config.js
+++ config/config.js
@@ -1,4 +1,7 @@
 module.exports = {
-    "URI": "mongodb+srv://shahriar:62rA9PkYzAtNz9wr@cluster0.abl9e.mongodb.net/SurveySite?retryWrites=true&w=majority",
-    "secret": "RgUkXp2s5v8y/B?E(G+KbPeShVmYq3t6w9z$C&F)J@McQfTjWnZr4u7x!A%D*G-K"
+    "URI": process.env.MONGODB,
+    "secret": process.env.SECRETKEY
+
+    // "URI": "mongodb+srv://shahriar:62rA9PkYzAtNz9wr@cluster0.abl9e.mongodb.net/SurveySite?retryWrites=true&w=majority",
+    // "secret": "RgUkXp2s5v8y/B?E(G+KbPeShVmYq3t6w9z$C&F)J@McQfTjWnZr4u7x!A%D*G-K"
 }
