9e1f9f1e4ec4ad75894884ca8157b10efdfc0a3f
Shahriar Moinuddin <shahriar.cmc@gmail.com>
2021-12-16 22:18:15 -0500

    other routing issue

---
+++ public/js/script.js
@@ -0,0 +1,19 @@
+/* COMP229 - Fall 2021 - - Sec 09
+Group - 13
+WEbExpert */
+
+function displayBlock(){
+    const inputArr = document.querySelectorAll('#divChoices input');
+    for(let i = 0; i < 10; i++){
+        if (inputArr[i].value == ""){
+            inputArr[i].style.display='block';
+            if (i == 9) {
+                document.getElementById('btn_addChoice').style.display='none';
+            }
+            break;
+        }
+    }
+}
+
+const btn = document.getElementById('btn_addChoice');
+btn.addEventListener('click', displayBlock);
